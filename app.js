fd5d13064186b8c6b86e7012500f383380b29631
Will Neal <willwneal@gmail.com>
2022-02-22 20:50:04 -0500

    Wrote if statement to add new department to table

--- app.js
+++ app.js
@@ -16,13 +16,40 @@ const db = mysql.createConnection(
     console.log(`Connected to the employees_db database....`)
 );
 
-mainMenu = [
+const mainMenu = [
     {
         name: "menu",
         type: "list",
         message: "What would you like to do?",
         choices: ["View all Departments", "View all Roles", "View all Employees", "Add a department", "Add a role", "Add an employee", "Update an employee Role", "Exit"]
     }
+]
+
+const department = [
+    {
+        name: "department",
+        type: "input",
+        message: "What is the name of the department?"
+    }
+]
+
+const role = [
+    {
+        name: "title",
+        type: "input",
+        message: "What is the name of the role?"
+    },
+    {
+        name: "Salary",
+        type: "number",
+        message: "What is the salary of the role?"
+    },
+    {
+        name: "deparment",
+        type: "list",
+        message: "Which Department does the role belong to?",
+        choices: ["Sales", "Engineering", "Finance", "Legal"]
+    },
 ]
 
 function appInit() {
@@ -47,6 +74,22 @@ function appInit() {
                 console.table(results);
                 appInit()
             }) 
+        } else if (data.menu === "Add a department") {
+            inquirer.prompt(department)
+            .then((data) => {
+                console.log(data.department)
+                db.query(`INSERT INTO department (name) VALUE ('${data.department}')`, (err, results) => {
+                    if (err) console.log(err);
+                    console.log("Query complete...");
+                    console.log(results);
+                })
+                
+            })
+        } else if (data.menu === "Add a role") {
+            inquirer.prompt(role)
+            .then((data) => {
+                console.log(data)
+            })
         } else {
             process.exit()
         }
