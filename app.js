40f24b2f3f34050ed5d6533d560c8c517f48ffc9
Will Neal <willwneal@gmail.com>
2022-02-23 09:25:16 -0500

    Wrote feature to add new Role

--- app.js
+++ app.js
@@ -40,12 +40,12 @@ const role = [
         message: "What is the name of the role?"
     },
     {
-        name: "Salary",
+        name: "salary",
         type: "number",
         message: "What is the salary of the role?"
     },
     {
-        name: "deparment",
+        name: "department",
         type: "list",
         message: "Which Department does the role belong to?",
         choices: ["Sales", "Engineering", "Finance", "Legal"]
@@ -77,18 +77,44 @@ function appInit() {
         } else if (data.menu === "Add a department") {
             inquirer.prompt(department)
             .then((data) => {
-                console.log(data.department)
                 db.query(`INSERT INTO department (name) VALUE ('${data.department}')`, (err, results) => {
                     if (err) console.log(err);
                     console.log("Query complete...");
-                    console.log(results);
+                    console.log(`${data.department} successfully added to departments...`);
                 })
                 
             })
         } else if (data.menu === "Add a role") {
             inquirer.prompt(role)
             .then((data) => {
-                console.log(data)
+                // console.log(data)
+                // console.log(data.title)
+                // console.log(data.salary)
+                // console.log(data.department)
+                departmentNum = 0
+                if (data.department === "Engineering") {
+                    departmentNum = 1;
+                } else if (data.department === "Finance"){
+                    departmentNum = 2;
+                } else if (data.department === "Legal") {
+                    departmentNum = 3;
+                } else if (data.department === "Sales") {
+                    departmentNum = 4;
+                } else {
+                    departmentNum = 5;
+                }
+                num = data.salary;
+                console.log(num)
+                stringDecimal = num.toFixed(2);
+                salary = Number(stringDecimal)
+                console.log(departmentNum)
+                
+                
+                db.query(`INSERT INTO roles (title, salary, department_id) VALUE ('${data.title}', '${data.salary}', ${departmentNum})`, (err, results) => {
+                    if (err) console.log(err);
+                    console.log(results);
+                    console.log(`${data.title} successfully added to roles...`)
+                })
             })
         } else {
             process.exit()
